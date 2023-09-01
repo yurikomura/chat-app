1a5a57ed14d5a4c62efe9f8704b0724d1c128c88
yurikomura <kafuu1313@gmail.com>
2023-09-01 20:25:00 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
   devise_for :users
+<<<<<<< HEAD
 =======
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ↑↑上の記述は削除しましょう↑↑
 >>>>>>> parent of 208f848 (deviseに関するビューファイルの配置)
+=======
+  get 'messages/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ↑↑上の記述は削除しましょう↑↑
+>>>>>>> parent of 3d2599a (ユーザー編集機能の実装)
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
 end
