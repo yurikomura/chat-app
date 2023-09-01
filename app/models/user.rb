6348e863dfb303b9f077de98ad6a855d89d3bd0d
yurikomura <kafuu1313@gmail.com>
2023-09-01 20:29:17 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :name, presence: true
-  
+  validates :name, presence: true
+
 end
